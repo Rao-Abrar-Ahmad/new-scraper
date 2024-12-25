598186f1c9c005b4b8525f613230c88776c2f07c
Rao Abrar Ahmad <raoabrar629@gmail.com>
2024-12-25 14:58:38 +0500

    Build Try 4

--- src/components/SectionTwo.jsx
+++ src/components/SectionTwo.jsx
@@ -21,9 +21,10 @@ function SectionTwo() {
         setEntries(results.data);
       },
     });
-    console.log('File...',file);
+    //console.log('File...',file);
     //console.log('handleing the click');
   }
+  console.log(file)
   return (
     <>
       <section className="section--second">
